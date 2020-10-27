f9aafa69669b9a4baa547b4235d5f075e2c391d3
Mithun <mkallatan@gmail.com>
2020-10-26 23:58:59 -0400

    App header - added title

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp((MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.blue,
+    appBar: AppBar(
+      title: Text('Virtual Dice'),
+      backgroundColor: Colors.lightBlue,
+    ),
+  ))));
+}
